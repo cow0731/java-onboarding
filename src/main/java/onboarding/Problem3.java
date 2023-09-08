4bba9df36c88f0225ed6c2f09c2989c72c13349c
cow0731 <cow0731@gmail.com>
2023-09-08 11:00:04 +0900

    feat(Problem3): 1차 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,17 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                answer++;
+            if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+                answer++;
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(13));
+        System.out.println(solution(33));
     }
 }
